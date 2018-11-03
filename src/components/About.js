49b48883fdf7bfb6dd9037cbe629c108b3555a49
GermanRodrickson <germanrodrickson@gmail.com>
2018-11-03 12:00:18 +0100

    text exp

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
 //-------- Images
 
@@ -13,13 +13,14 @@ const Wrapper = styled.section`
   width: 100%;
   padding: 2rem 0;
   position: relative;
-`;
+`
 
 const Img = styled.img`
   width: 350px;
   height: 350px;
   display: inline-block;
-`;
+  border-radius: 50%;
+`
 
 
 const Title = styled.h2`
@@ -27,7 +28,7 @@ const Title = styled.h2`
   text-align: center;
   font-size: 3rem;
   color: #32363e;
-  margin: 40px 0px;
+  margin: 40px 0px 0px 0px;
 `
 
 const Text = styled.p`
@@ -37,12 +38,13 @@ const Text = styled.p`
   align-content: top;
   margin-top: 45px;
   line-height: 1.6;
-`;
+  margin-left: 140px;
+`
 
 const WrapperText = styled.div`
   display: flex;
   justify-content: center;
-`;
+`
 
 const Triange = styled.div`
   width: 0;
@@ -55,11 +57,11 @@ const Triange = styled.div`
   margin: 0 auto;
   right: 50%;
   margin-right: -40px;
-`;
+`
 
 const Word = styled.strong`
   color: #44bafc;
-`;
+`
 
 class About extends Component {
   render() {
@@ -68,11 +70,11 @@ class About extends Component {
         <Title>Bio</Title>
           <WrapperText>
             <Img src={profile} />
-          <Text>
-            Previously, I was working in a <Word>hospital</Word> where I was dealing with pressure on a daily basis. Looking to <Word>leave my comfort zone</Word> and learn new things, I dove into an unknown sector to start a new adventure and <Word>grow as a person.</Word>
-            <br /> <br />
-            I'm known to be hands-on, dynamic, communicative, enthusiastic, eager to learn, easy going and a team player.
-          </Text>
+            <Text>
+              Previously, I was working in a <Word>hospital</Word> where I was dealing with pressure on a daily basis. Looking to <Word>leave my comfort zone</Word> and learn new things, I dove into an unknown sector to start a new adventure and <Word>grow as a person.</Word>
+              <br /> <br />
+              I'm known to be hands-on, dynamic, communicative, enthusiastic, eager to learn, easy going and a team player.
+            </Text>
         </WrapperText>
       </Wrapper>;
   }
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+//-------Images
+
+import nodejs from "../img/nodejs.png";
+
+//-------
+
 
 const Wrapper = styled.section`
   background-color: #fdfdfd;
   width: 100%;
   padding: 2rem 0;
 `;
-
 
 const Title = styled.h2`
   letter-spacing: 2px;
@@ -27,14 +32,12 @@ const Text = styled.p`
 const ColumnHosp = styled.div`
   background-color: #FFFFFF;
   width: 50%;
-  height: 100px;
   display: inline-block;
   font-size: 0px;
 `;
 const ColumnDev = styled.div`
   background-color: #32363e;
   width: 50%;
-  height: 100px ;
   display: inline-block;
   font-size: 0px;
 `;
@@ -47,6 +50,27 @@ const SecondaryTitle = styled.h3`
   margin: 20px 0px;
 `;
 
+const Works = styled.h4`
+  color: #bec6d5;
+  text-align: center;
+  margin: 0 auto;
+  font-size: 1.5rem;
+`;
+
+const WrapperText = styled.div `
+  display: flex;
+  justify-content: center;
+`
+
+const Img = styled.img`
+  width: 50px;
+  height: 50px;
+  float: left;
+`;
+
+
+
+
 
 
 class Experience extends Component {
@@ -55,6 +79,10 @@ class Experience extends Component {
         <Title>Experience</Title>
         <ColumnDev>
           <SecondaryTitle>As a Developer <span role="img" aria-label="laptop">💻</span></SecondaryTitle>
+            <Works>Barcelona Virtual</Works>
+            <Text>Frontend Developer</Text>
+            <Text>This is my firts job as a developer, we are using php, sass and now we are implementing React.</Text>
+            <Img src={nodejs}></Img>
         </ColumnDev>
         <ColumnHosp>
           <SecondaryTitle>As a medical imaging technician <span role="img" aria-label="hospital">🏥</span></SecondaryTitle>
