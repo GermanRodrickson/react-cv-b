61db9830a6e3fd6b2ba5e59f3af0b6149a56aaaa
GermanRodrickson <germanrodrickson@gmail.com>
2018-11-25 17:01:04 +0100

    line with circle

--- src/components/Experience2.js
+++ src/components/Experience2.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+const Wrapper = styled.section`
+  position: relative;
+  height: 400px;
+`
 
+const Line = styled.div`
+  position: absolute;
+  height: 300px;
+  width: 2px;
+  top: 0;
+  right: 50%;
+  margin-left: -1px;
+  background-color: red;
+`
+
+const Circle = styled.div`
+  position: absolute;
+  border-radius: 100%;
+  width: 12px;
+  height: 12px;
+  top: 0;
+  right: 50%;
+  margin-right: -5px;
+  background-color: red;
+`;
 
 class Experience2 extends Component {
   render() {
-    return(<div>Hello</div>)
+    return(
+    <Wrapper>
+      <Circle />
+      <Line />
+    </Wrapper>)
   }
 }
 
